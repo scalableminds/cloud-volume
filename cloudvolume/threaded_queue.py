080422b96479d0c4150f02d912fdac3ed6830a25
Norman Rzepka <code@normanrz.com>
2019-01-07 12:12:09 +0100

    less sleep

--- cloudvolume/threaded_queue.py
+++ cloudvolume/threaded_queue.py
@@ -223,7 +223,7 @@ class ThreadedQueue(object):
           pbar.update(delta)
         last = size
         self._check_errors()
-        time.sleep(0.1)
+        time.sleep(1e-9)
 
       # Wait until all tasks in the queue are 
       # fully processed. queue.task_done must be
